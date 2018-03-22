f992782b79bce655972fc7a8bbdb429da7143bbb
raymond69 <raymond.packa@orange.com>
2018-03-22 17:17:00 +0100

    Ajout caméra

--- flutter_test_app/lib/home.dart
+++ flutter_test_app/lib/home.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'dart:async';
+
+List<CameraDescription> cameras;
+
+Future<Null> initCamera() async {
+  cameras = await availableCameras();
+}
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -8,16 +16,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CameraController controller;
 
   @override
   void initState() {
     super.initState();
+    initCamera();
+
+    controller = new CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-
-    );
+    if (!controller.value.initialized) {
+      return new Container();
+    }
+    return new AspectRatio(
+        aspectRatio:
+        controller.value.aspectRatio,
+        child: new CameraPreview(controller));
   }
 }
